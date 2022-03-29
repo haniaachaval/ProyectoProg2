5743f1257330060104f78ca8761906fe2e8766b1
haniaachaval <hachavalrodriguez@udesa.edu.ar>
2022-03-29 15:05:59 -0300

    cambiohania

---
+++ proyectoIntegrador2/db/libros.js
@@ -0,0 +1,82 @@
+let celulares = {
+    lista: [
+        {
+            marca: "iphone",
+            modelo: "iphone 11",
+            estado: "nuevo",
+            color: "black",
+        },
+        {
+            marca: "iphone",
+            modelo: "iphone 11",
+            estado: "usado",
+            color: "white",
+        },
+        {
+            marca: "iphone",
+            modelo: "iphone 8 plus",
+            estado: "nuevo",
+            color: "white",
+        },
+        
+        {
+            marca: "iphone",
+            modelo: "iphone 8 plus",
+            estado: "usado",
+            color: "black",
+        },
+        {
+            marca: "iphone",
+            modelo: "iphone X",
+            estado: "usado",
+            color: "",
+        },
+        {
+            marca: "iphone",
+            modelo: "iphone X",
+            estado: "usado",
+            color: "blue",
+        },
+
+
+        {
+            marca: "samsung",
+            modelo: "samsung galaxy S21",
+            estado: "nuevo",
+            color: "black",
+        },
+        {
+            marca: "samsung",
+            modelo: "samsung galaxy S21",
+            estado: "usado",
+            color: "red",
+        },{
+            marca: "samsung",
+            modelo: "samsung galaxy Z Fold 3",
+            estado: "usado",
+            color: "white",
+        },
+        {
+            marca: "samsung",
+            modelo: "samsung galaxy Z Fold 3",
+            estado: "nuevo",
+            color: "black",
+        },
+        {
+            marca: "samsung",
+            modelo: "samsung galaxy S20",
+            estado: "nuevo",
+            color: "black",
+        },
+        {
+            marca: "samsung",
+            modelo: "samsung galaxy S20",
+            estado: "usado",
+            color: "white",
+        },
+        
+      
+    ]
+}
+
+module.exports = celulares
