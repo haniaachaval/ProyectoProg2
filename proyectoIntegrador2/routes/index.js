6a205dbfc845324d6db199ed4063c7295e58a4df
Cande Di Chiara <cdichiara@udesa.edu.ar>
2022-04-06 16:58:18 -0300

    me lo pide para hacer pull

--- proyectoIntegrador2/routes/index.js
+++ proyectoIntegrador2/routes/index.js
@@ -2,8 +2,6 @@ const express = require('express');
 const router = express.Router();
 // lo de arriba permite modularizar el sistema de ruteo. 
 
-const celulares = require('../db/celulares.js')
-
 const controlador = require ('../controllers/indexController');
 
 router.get('/', controlador.lista);
