50357d6f42e298832c6a85424f160b42fd8cac80
Cande Di Chiara <cdichiara@udesa.edu.ar>
2022-04-11 17:45:58 -0300

    resultados de busqueda

--- proyectoIntegrador2/controllers/indexController.js
+++ proyectoIntegrador2/controllers/indexController.js
@@ -4,7 +4,10 @@ let usuario = require ("../db/usuario")
 const indexController = {
     home : function(req,res){
         res.render ('index',{celus: db.lista })
-    }
+    },
+    resultados: function(req,res){
+        res.render('search-results')
+    },
 }
 
 module.exports = indexController
--- proyectoIntegrador2/routes/index.js
+++ proyectoIntegrador2/routes/index.js
@@ -5,8 +5,7 @@ const router = express.Router();
 const controlador = require ('../controllers/indexController');
 
 router.get('/', controlador.home);
-
-
+router.get('/resultado', controlador.resultados);
 
 
 module.exports = router;
