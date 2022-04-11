edadde0da0ff973ad86779088fa68f8478eef58f
tizianasordini <tizianasordini@hotmail.com>
2022-04-11 02:17:34 -0300

    comentarios dinamicos

--- proyectoIntegrador2/controllers/productoController.js
+++ proyectoIntegrador2/controllers/productoController.js
@@ -1,11 +1,12 @@
 const celulares = require("../db/celulares");
+const comentario = require("../db/comentario")
 
 const productoController = {
     agregarProducto : function(req,res){
         res.render ('product-add', {logueado : false});
 },
     detalleProducto : function(req,res){
-        res.render ('producto', { celulares: celulares.lista, logueado:false });
+        res.render ('producto', { celulares: celulares.lista, logueado:false, comentario: comentario.lista });
 }
 }
 
