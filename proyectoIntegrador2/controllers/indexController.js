109e70a54b9bff7e33837e7fb3ebe06c0d989818
tizianasordini <tizianasordini@hotmail.com>
2022-06-13 17:21:34 -0300

    search

--- proyectoIntegrador2/controllers/indexController.js
+++ proyectoIntegrador2/controllers/indexController.js
@@ -1,8 +1,24 @@
-const db = require("../db/celulares")
+const db = require("../database/models")
+const celular = require("../db/celulares")
+const {op} = require ("sequelize");
 
 const indexController = {
     home : function(req,res){
-        return res.render ('index',{celus: db.lista })
+        return res.render ('index',{celus: db.Product })
+    },
+    resultados : function(req, res){
+        let search = req.query.search
+        db.Product.findAll({
+            where : [{name: {[op.like]: "%" + search + "%"}}]
+        })
+        .then((data) => {
+            return res.render ("search-results", {
+                product: data,
+            })
+            .catch((error) => {
+                return res.send(error);
+            })
+        })
     },
     resultados: function(req,res){
         return res.render('search-results')
